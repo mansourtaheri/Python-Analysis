e985785106727310633ca4afaeaae60769ab6437
mansourtaheri <93048987+mansourtaheri@users.noreply.github.com>
2021-10-23 11:34:15 -0500

    Update main.py
    read data

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import csv
 
-# Path of the CSV file:
 input_path = "C:/Users/Mansour/Desktop/python-challenge/tank_data.csv"
 
-# Path of the output text file
+
 output_path = "C:/Users/Mansour/Desktop/python-challenge/PyBank_results.txt"
+
+thermal_rows = []
+input_rows = []
+
+
+with open(input_path) as csv_file:
+    read_file = csv.reader(csv_file , delimiter = ",")
+    header_file = next(read_file)
+    for row in read_file:
+        input_rows.append(row[0])
+        thermal_rows.append(int(row[1]))
