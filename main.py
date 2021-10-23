666e5df0d3ac3d68a76081983f7bc7e075a49bce
mansourtaheri <93048987+mansourtaheri@users.noreply.github.com>
2021-10-23 11:30:06 -0500

    Create main.py
    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import csv
+
+# Path of the CSV file:
+input_path = "C:/Users/Mansour/Desktop/python-challenge/tank_data.csv"
+
+# Path of the output text file
+output_path = "C:/Users/Mansour/Desktop/python-challenge/PyBank_results.txt"
