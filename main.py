b421e76fe056ecf881872fff5263310616d82c54
mansourtaheri <93048987+mansourtaheri@users.noreply.github.com>
2021-10-23 11:38:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ with open(input_path) as csv_file:
     for row in read_file:
         input_rows.append(row[0])
         thermal_rows.append(int(row[1]))
+
+
+number_of_month = len(input_rows)
+
+sum_of_temps = sum(thermal_rows)
+
+average_change = round((thermal_rows[-1]-thermal_rows[0])/(number_of_month-1),2)
+
+fifferent_temps = [j - i for i, j in zip(thermal_rows[: -1], thermal_rows[1 :])] 
+
+greatest_increase = max(fifferent_temps)
+ind_increase = fifferent_temps.index(greatest_increase)+1
+date_increase = input_rows[ind_increase]
+
+greatest_decrease= min(fifferent_temps)
+ind_decrease = fifferent_temps.index(greatest_decrease)+ 1
+date_decrease = input_rows[ind_decrease]
+
+
+with open(output_path,'w') as output:
+   output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format("Thermal Analysis","----------------------------",f"Total Months: {number_of_month}",
+   f"Total: ${sum_of_temps}",f"Average  Change: ${average_change}",f"Greatest Increase in thermal: {date_increase} (${greatest_increase})",
+   f"Greatest Decrease in thermal: {date_decrease} (${greatest_decrease})","----------------------------"))
+ 
+    
+    
+    
